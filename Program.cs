b5a4d38f628a7dbd11f177347bee8aa07323cbac
Rita <lihachyowa.rita@yandex.ru>
2022-06-23 14:19:21 +0300

    Задача 4

--- Program.cs
+++ Program.cs
@@ -57,3 +57,21 @@ else
     Console.WriteLine(a + " нечетное");
 */
 
+
+/* Задача 4
+int a, i;
+
+Console.Write("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+
+i = 1;
+
+while (i <= a)
+{
+    if (i%2 == 0)
+    {   
+        Console.Write(i + " ");
+    }
+    i++;
+}
+*/
