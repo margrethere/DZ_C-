d77cbf83e8d0535e07d074468eaadfa9bd88917b
Rita <lihachyowa.rita@yandex.ru>
2022-06-23 14:07:00 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 1
+
+int a = 5;
+int b = 7;
+
+int max = 7;
+
+if (a > max) max = 5;
+if (b > max) max = 7;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+/* Задача 2
+int a, b, c, max;
+
+Console.WriteLine("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    max = a;
+}
+else
+    max = b;
+
+if (max > c)
+{
+    Console.WriteLine("Максимальное число: " + max);
+}
+else 
+if (max < c)
+{
+    max = c;
+    Console.WriteLine("Максимальное число: " + max);
+}
+*/ 
